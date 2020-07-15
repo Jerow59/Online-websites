8de67b7eaed03c00e2a214966eaecc5d2d18ff47
Jerow <56309127+Jerow59@users.noreply.github.com>
2020-07-15 16:14:26 +0200

    Add files via upload

---
+++ Front-End/My-Jungle-Room/js/Effects.js
@@ -0,0 +1,146 @@
+"use strict";
+
+$.fn.moveIt = function () {
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function () {
+    instances.push(new moveItItem($(this)));
+  });
+
+  // Modif opacité au scroll
+  window.onscroll = function () {
+    var scrollTop = $window.scrollTop();
+    let opacity = 1 - (scrollTop / 250);
+    if (opacity >= 0) {
+      $('.baseline-name').css('opacity', opacity);
+      $('.downArrow').css('opacity', opacity);
+    }
+    instances.forEach(function (inst) {
+      inst.update(scrollTop);
+    });
+
+    // Bar de progression de lecture
+    var height = document.body.scrollHeight;
+    var windowHeight = window.innerHeight;
+
+    var trackLenght = height - windowHeight;
+    var percentage = (scrollTop / trackLenght) * 100;
+    document.getElementById('progress-bar').style.right = 100 - percentage + "%";
+  }
+}
+
+// Effet "parallaxe"
+var moveItItem = function (el) {
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function (scrollTop) {
+  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+// Initialisation
+$(function () {
+  $('[data-scroll-speed]').moveIt();
+});
+
+
+// Popup
+
+var contenuConseilsNom = document.getElementById('contenuConseilsNom');
+var contenuConseilsExposition = document.getElementById('contenuConseilsExposition');
+var contenuConseilsArrosage = document.getElementById('contenuConseilsArrosage');
+
+var blur = document.getElementById('blur');
+var popup = document.getElementById('popup');
+var fermer = document.getElementById('fermer');
+
+
+$('.home-section span').click(function () {
+  var contenu = String($(this).attr('id'));
+  //console.log("le contenu id du bouton :" + contenu)
+
+
+  function positionsDuClic(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    //console.log("La position du clic est x:" + x + "px et y:" + y + "px");
+
+    var widthWindow = $(window).width();
+    //console.log("La largeur de la fenêtre est de " + widthWindow);
+
+    var heightWindow = $(window).height();
+    //console.log("La hauteur de la fenêtre est de " + heightWindow);
+
+    var widthPopup = popup.offsetWidth;
+    //console.log("La largeur de la popup est de " + widthPopup);
+
+    var heightPopup = popup.offsetHeight;
+    //console.log("La hauteur de la popup est de " + heightPopup);
+
+    var differenceWidthEntreWindowEtPopup = Number(widthWindow - widthPopup);
+    //console.log("La différence de largeur entre la fenêtre et la popup est de " + differenceWidthEntreWindowEtPopup);
+
+    var differenceHeightEntreWindowEtPopup = Number(heightWindow - heightPopup);
+    //console.log("La différence de hauteur entre la fenêtre et la popup est de " + differenceHeightEntreWindowEtPopup);
+
+    var differenceWidthEntreWindowEtPopupCote = differenceWidthEntreWindowEtPopup / 2;
+    //console.log("La différence de largeur sur les cotés entre full-popup et popup est de " + differenceWidthEntreWindowEtPopupCote);
+
+    var differenceHeightEntreWindowEtPopupCote = differenceHeightEntreWindowEtPopup / 2;
+    //console.log("La différence de hauteur sur les cotés entre full-popup et popup est de " + differenceHeightEntreWindowEtPopupCote);
+
+    var xPop = Number;
+    var yPop = Number;
+
+    if (x < differenceWidthEntreWindowEtPopupCote) {
+      xPop = (- (differenceWidthEntreWindowEtPopupCote - x))
+    } else {
+      xPop = (x - differenceWidthEntreWindowEtPopupCote)
+    }
+
+    if (y < differenceHeightEntreWindowEtPopupCote) {
+      yPop = (- (differenceHeightEntreWindowEtPopupCote - y))
+    } else {
+      yPop = (y - differenceHeightEntreWindowEtPopupCote)
+    }
+
+    //console.log("La valeur de xPop est: " + xPop);
+    //console.log("La valeur de yPop est: " + yPop);
+
+
+    popup.animate([
+      // keyframes
+      { clipPath: 'circle(0px at ' + xPop + 'px ' + yPop + 'px)' },
+      { clipPath: 'circle(500% at ' + xPop + 'px ' + yPop + 'px)' }
+    ], {
+      // timing options
+      duration: 500,
+      //iterations: 1
+    });
+
+  }
+
+  var positions = positionsDuClic(event);
+
+  blur.classList.toggle('active');
+  popup.classList.toggle('active');
+
+  for (let i = 0; i <= dataPlantes.length; i++) {
+    if (contenu === dataPlantes[i].nom) {
+      //console.log("les noms sont identitiques avec " + dataPlantes[i].nom)
+      contenuConseilsNom.innerHTML = dataPlantes[i].nomPlante
+      contenuConseilsExposition.innerHTML = dataPlantes[i].exposition
+      contenuConseilsArrosage.innerHTML = dataPlantes[i].arrosage
+    } else {
+      //console.log("les noms sont différents avec " + dataPlantes[i].nom)
+    }
+  }
+});
+
+$('#fermer').click(function () {
+  //event.preventDefault();
+  blur.classList.toggle('active');
+  popup.classList.toggle('active');
+})
---
+++ Front-End/My-Jungle-Room/js/dataEntretien.js
@@ -0,0 +1,45 @@
+var dataPlantes =
+  [
+    {
+        "nom": "conseilsDieffenbachia",
+        "nomPlante": "Dieffenbachia",
+        "numero": "1",
+        "exposition": "Lumière vive, le plus près possible d'une fenêtre mais sans soleil direct excepté en plein hiver.",
+        "arrosage": "Arrosez régulièrement mais prenez soin de laisser sécher le mélange en surface entre deux arrosages."
+    },
+    {
+        "nom": "conseilsChlorophytum",
+        "nomPlante": "Chlorophytum",
+        "numero": "2",
+        "exposition": "En extérieur : ombre ou mi-ombre ; En intérieur, prévoyez un minimum de luminosité sans soleil direct.",
+        "arrosage": "Arrosez régulièrement durant l'été, mais sans excès : le chlorophytum supporte beaucoup mieux la sécheresse que l'excès d'eau."
+    },
+    {
+        "nom": "conseilsSyngonium",
+        "nomPlante": "Syngonium",
+        "numero": "3",
+        "exposition": "Le syngonium se satisfait d'une lumière tamisée. Il ne tolère pas le plein soleil qui risque de brûler son feuillage.",
+        "arrosage": "Laissez le mélange sécher sur quelques centimètres avant d'arroser à nouveau avec de l'eau à température ambiante. Ne laissez jamais d'eau stagner dans la coupelle."
+    },
+    {
+        "nom": "conseilsSchefflera",
+        "nomPlante": "Schefflera",
+        "numero": "4",
+        "exposition": "Il s'accommodera sans souci d'une pièce moyennement lumineuse, même s'il se plaît à la lumière, sous laquelle il se développera plus rapidement. Il supportera quelques heures de soleil à proximité d'une vitre.",
+        "arrosage": "Arrosez parcimonieusement en laissant sécher la terre entre 2 arrosages, mais augmentez l'humidité athmosphérique en hiver en brumisant le feuillage une à deux fois par semaine en hiver, plus souvent en été."
+    },
+    {
+        "nom": "conseilsPilea",
+        "nomPlante": "Pilea",
+        "numero": "5",
+        "exposition": "Très lumineuse. Le soleil direct semble pouvoir être supporté après une période d’acclimatation. Les feuilles tendent vers la lumière, tournez le pot de temps en temps pour conserver un port équilibré à la plante.",
+        "arrosage": "Arrosages réguliers mais l'eau ne doit pas stagner. Un apport d'engrais n'est pas nécessaire, du moins pas régulièrement."
+    },
+    {
+        "nom": "conseilsMarantaLeuconeura",
+        "nomPlante": "Maranta leuconeura 'Fascinator'",
+        "numero": "6",
+        "exposition": "La Maranta leuconeura 'Fascinator' se plaira à l’ombre ou à la mi-ombre.",
+        "arrosage": "Arrosez-la une fois par semaine au printemps, une à deux fois par semaine en été et une fois tous les 10 jours à 14 jours en automne et hiver."
+    }]
+  ;
